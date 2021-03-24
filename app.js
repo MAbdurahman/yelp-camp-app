c35acb78367ace1547581d0fec4cc220410268e9
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-24 07:48:51 -0500

    Complete basic campground model

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Campground = require('./models/campgrounds');
 
 //**************** variables ****************//
 const app = express();
 const port = process.env.Port || 3000;
+
+//**************** database connection ****************//
+mongoose
+	.connect('mongodb://localhost:27017/yelp-camp', {
+		useNewUrlParser: true,
+      useCreateIndex: true,
+		useUnifiedTopology: true,
+	});
+
+   const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', () => {
+		console.log('mongodb connected with mongoose...');
+	});
+
 
 
 
@@ -23,6 +39,14 @@ app.get('/', (req, res) => {
 	res.render('home');
 	res.statusCode = 200;
 	console.log('home request...');
+});
+app.get('/makecampground', async (req, res) => {
+   const camp = new Campground({
+      title: 'Backyard Camp',
+      description: 'Roughing it at home, under the stars.'
+   })
+   await camp.save();
+   res.send(camp);
 });
 
 
