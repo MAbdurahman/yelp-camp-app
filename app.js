0acf787717579af1417725b15c4dd5f110516694
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-24 06:07:19 -0500

    Add entry point app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//**************** imports ****************//
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+//**************** variables ****************//
+const app = express();
+const port = process.env.Port || 3000;
+
+
+
+//**************** app routes ****************//
+app.get('/', (req, res) => {
+	res.send(`<h1>yelp-camp-app</h1>`);
+	res.statusCode = 200;
+	console.log('add product request...');
+});
+
+
+
+
+
+//**************** app listening ****************//
+app.listen(port, () => {
+	console.log(`app listening at http://localhost:${port}`);
+});
